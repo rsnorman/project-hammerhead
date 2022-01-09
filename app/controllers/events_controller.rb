b27767bbc92e5bed2fac3b47bf49424f3ef537ae
Ryan Norman <rnorman@covermymeds.com>
2022-01-09 13:22:48 -0500

    Sorts events with most recent at top

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = Event.all
+    @events = Event.all.sort { |a, b| b.created_at <=> a.created_at }
   end
 
   # GET /events/1 or /events/1.json
